895a8a0e8232fcac302fce4814169b0ce32d0803
lxzxiangxiang <2124198496@qq.com>
2026-05-27 13:31:45 +0800

    clean: remove untitled project and build artifacts

--- untitled/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled_FILE D:/code/project/0.0.1/untitled/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/untitled.exe)
-set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_untitled_RUNTIME_DLLS D:/anzhuangweizhi/qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;D:/anzhuangweizhi/qt/6.11.0/mingw_64/bin/Qt6Gui.dll;D:/anzhuangweizhi/qt/6.11.0/mingw_64/bin/Qt6Core.dll)
